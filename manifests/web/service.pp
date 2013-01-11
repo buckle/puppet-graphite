719e9df35f654d906eddee3cc1453f4d364c01c8
Justin Ellison <justin.ellison@buckle.com>
2013-01-11 16:59:59 -0600

    firewall fixes

--- manifests/web/service.pp
+++ manifests/web/service.pp
@@ -1,11 +1,10 @@
 class graphite::web::service {
-
+  Class['graphite::web::package'] ~> Class['graphite::web::service']
+  include bke_firewall::http_server
   service { 'httpd':
     ensure     => running,
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
   }
-
-
 }
