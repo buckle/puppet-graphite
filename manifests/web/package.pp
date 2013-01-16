0b697a1c8b34c88a4ee3af9d77a6ec17b1abd097
Justin Ellison <justin.ellison@buckle.com>
2013-01-15 18:36:31 -0600

    Include rrdtool-python to allow graphite to work with legacy rrds

--- manifests/web/package.pp
+++ manifests/web/package.pp
@@ -12,6 +12,9 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class graphite::web::package {
+  # This allows us to read rrd's from graphite
+  realize(Package['rrdtool-python'])
+
   package { ['bitmap-fonts-compat', 'graphite-web']:
     ensure  => present,
     require => Yumrepo['bke'],
