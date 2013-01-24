7b7f2a24e9b39f5c83832bf089ebbb8f685b4470
Justin Ellison <justin.ellison@buckle.com>
2013-01-23 18:56:35 -0600

    fix for idiocy in build-index.sh

--- manifests/web/cron.pp
+++ manifests/web/cron.pp
@@ -2,7 +2,7 @@ class graphite::web::cron {
   cron { 'Graphite search index update':
     ensure      => present,
     command     => '/usr/bin/build-index.sh',
-    environment => 'GRAPHITE_STORAGE_DIR=/var/lib/carbon/',
+    environment => 'GRAPHITE_STORAGE_DIR=/var/lib/graphite-web/',
     minute      => '*/30'
   }
 }
