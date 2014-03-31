582227cd71705b1da981e39190c643c4ceb232d6
Justin Ellison <justin.ellison@buckle.com>
2014-03-31 14:39:54 -0500

    adding logrotate support

--- manifests/web.pp
+++ manifests/web.pp
@@ -14,16 +14,22 @@
 #
 # * Update documentation
 #
-class graphite::web (
-  $manage_httpd = false
-  ) {
+class graphite::web ($manage_httpd = false) {
   include graphite::web::package
   include graphite::web::config
   include graphite::web::cron
+
   if $graphite::web::manage_httpd {
     include graphite::web::service
   }
+
+  logrotate::rule { 'graphite-web':
+    path          => '/var/log/graphite-web/*.log',
+    missingok     => true,
+    ifempty       => false,
+    sharedscripts => true,
+    delaycompress => true,
+    postrotate    => '/sbin/service httpd reload > /dev/null 2>/dev/null || true',
+  }
 }
-
-
 
